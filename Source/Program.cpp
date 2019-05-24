27f01a555adcd870eb4a4570cd76f08654154f42
ARM32 <arm32.x@gmail.com>
2019-05-24 14:21:13 -0400

    Automatic commit by Code::Blocks.

--- Source/Program.cpp
+++ Source/Program.cpp
@@ -157,7 +157,7 @@ void Program::main(int argc, char** argv) {
         Time frameTime = frameClock.restart();
         float delta = frameTime / optimalTime;
         int substeps = 1;
-        while (delta >= 2) {
+        while (delta >= 1.25f) {
             delta    /= 2;
             substeps *= 2;
         }
