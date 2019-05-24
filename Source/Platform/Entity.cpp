65cec2ebde9acc83cc550900c9704f4115031743
ARM32 <arm32.x@gmail.com>
2019-05-24 17:45:29 -0400

    Automatic commit by Code::Blocks.

--- Source/Platform/Entity.cpp
+++ Source/Platform/Entity.cpp
@@ -67,10 +67,15 @@ void Entity::update(float delta) {
                 if (mode == CollisionMode::Solid || mode == CollisionMode::SolidFloor) {
                     // TODO:  Implement this.
                     Program::log(Log::Debug, "EntityCollision") << "Horizontal collision detected." << std::endl;
-                    move(0.0f, -amountMoved.y - velocity.y >= 0.0f ? -0.125f : 0.125f);
+                    move(0.0f, -amountMoved.y);
                     for (amountMoved.x = 0.0f; (mode == CollisionMode::Solid || mode == CollisionMode::SolidFloor) && std::abs(amountMoved.x) <= std::abs(velocity.x); amountMoved.x += velocity.x >= 0.0f ? -0.125f : 0.125f) {
                         move(velocity.x >= 0.0f ? -0.125f : 0.125f, 0.0f);
                         setVelocity(0.0f, velocity.y);
+                        mode = collider->prioritizeTileModes(collider->getTilesTouching(*this));
+                    }
+                    for (amountMoved.y = 0.0f; (mode == CollisionMode::Solid || mode == CollisionMode::SolidFloor) && std::abs(amountMoved.y) <= std::abs(velocity.y); amountMoved.y += velocity.y >= 0.0f ? -0.125f : 0.125f) {
+                        move(0.0f, velocity.y >= 0.0f ? -0.125f : 0.125f);
+                        setVelocity(velocity.x, 0.0f);
                         mode = collider->prioritizeTileModes(collider->getTilesTouching(*this));
                     }
                 }
