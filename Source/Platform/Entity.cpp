5548b6996b41c602c8ffc8329ffecb779dfaabdb
ARM32 <arm32.x@gmail.com>
2019-06-11 10:13:55 -0400

    Automatic commit by Code::Blocks.

--- Source/Platform/Entity.cpp
+++ Source/Platform/Entity.cpp
@@ -53,10 +53,10 @@ void Entity::update(float delta) {
     auto getCollisionMode = [this]() {
         std::vector<CollisionMode> modes;
         for (TilemapCollider* collider : TilemapCollider::all) {
-            if (getPosition().x > collider->tilemap.getPosition().x + collider->tilemap.width  * collider->tilemap.tileSize) continue;
-            if (getPosition().y > collider->tilemap.getPosition().y + collider->tilemap.height * collider->tilemap.tileSize) continue;
-            if (getPosition().x + getTextureRect().width  < collider->tilemap.getPosition().x) continue;
-            if (getPosition().y + getTextureRect().height < collider->tilemap.getPosition().y) continue;
+            //if (getPosition().x > collider->tilemap.getPosition().x + collider->tilemap.width  * collider->tilemap.tileSize) continue;
+            //if (getPosition().y > collider->tilemap.getPosition().y + collider->tilemap.height * collider->tilemap.tileSize) continue;
+            //if (getPosition().x + getTextureRect().width  < collider->tilemap.getPosition().x) continue;
+            //if (getPosition().y + getTextureRect().height < collider->tilemap.getPosition().y) continue;
             std::vector<Vector2<uint16_t>> tiles = collider->getTilesTouching(*this);
             CollisionMode mode = collider->prioritizeTileModes(tiles);
             if (mode != CollisionMode::NotTouching) modes.push_back(mode);
