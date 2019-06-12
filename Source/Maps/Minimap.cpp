82d9d4a992e9141f9a94434a7b3b26345f2849e5
ARM32 <arm32.x@gmail.com>
2019-06-12 18:16:16 -0400

    Automatic commit by Code::Blocks.

--- Source/Maps/Minimap.cpp
+++ Source/Maps/Minimap.cpp
@@ -19,9 +19,15 @@ Minimap::Minimap(uint16_t width, uint16_t height)
 }
 
 void Minimap::draw(RenderTarget& target, RenderStates states) const {
+    static Vertex background[4] {
+        Vertex(Vector2f( 0.0f,  0.0f), Color::Black),
+        Vertex(Vector2f(56.0f,  0.0f), Color::Black),
+        Vertex(Vector2f(56.0f, 56.0f), Color::Black),
+        Vertex(Vector2f( 0.0f, 56.0f), Color::Black)
+    };
     View oldView = target.getView();
     target.setView(view);
-    target.clear();
+    target.draw(background, 4, PrimitiveType::Quads, states);
     Tilemap::draw(target, states);
     target.setView(oldView);
 }
