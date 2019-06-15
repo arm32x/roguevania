c717df011041b0e7919a9f0a905a2178a584968a
ARM32 <arm32.x@gmail.com>
2019-06-14 21:14:38 -0400

    Automatic commit by Code::Blocks.

--- Source/Platform/Player.cpp
+++ Source/Platform/Player.cpp
@@ -5,13 +5,13 @@ using namespace Roguevania::Platform;
 using namespace sf;
 
 void Player::update(float delta) {
-    if (jumping && getVelocity().y < 0.0f) accelerate(0.0f, -0.15f);
+    if (jumping && getVelocity().y < 0.0f) accelerate(0.0f, -0.15f * delta);
     Entity::update(delta);
     
     // Handle movement controls.
-    /**/ if (Keyboard::isKeyPressed(Keyboard::D)) accelerate(( 2.0f - getVelocity().x) / 4.0f, 0.0f);
-    else if (Keyboard::isKeyPressed(Keyboard::A)) accelerate((-2.0f - getVelocity().x) / 4.0f, 0.0f);
-    else /**************************************/ accelerate(getVelocity().x / -4.0f, 0.0f);
+    /**/ if (Keyboard::isKeyPressed(Keyboard::D)) accelerate(( 2.0f - getVelocity().x) / 4.0f * delta, 0.0f);
+    else if (Keyboard::isKeyPressed(Keyboard::A)) accelerate((-2.0f - getVelocity().x) / 4.0f * delta, 0.0f);
+    else /**************************************/ accelerate(getVelocity().x / -4.0f * delta, 0.0f);
 }
 
 void Player::event(const Event& e) {
