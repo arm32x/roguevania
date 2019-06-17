224e030528ae151c0fd04c5e43adfc4c0da31430
ARM32 <arm32.x@gmail.com>
2019-06-17 13:58:07 -0400

    Automatic commit by Code::Blocks.

--- Source/Program.cpp
+++ Source/Program.cpp
@@ -36,6 +36,7 @@ using stx::nullopt;
 #define WINDOWED_SCALE 0
 #define CAMERA_MODE 1
 #define ROOM_TEST_MODE 0
+#define GAME_LOOP_MODE 1
 
 #if WINDOWED_SCALE == 0
 RenderWindow Program::window(VideoMode(VideoMode::getDesktopMode().width, VideoMode::getDesktopMode().width * (9.0 / 16.0)), "Roguevania (working title)", Style::Fullscreen);
@@ -48,11 +49,11 @@ Log Program::log("Roguevania", Log::Debug);
 void Program::main(int argc, char** argv) {
     window.setVerticalSyncEnabled(true);
     window.setView(camera.view);
-    #ifdef _WIN32
+#ifdef _WIN32
     // Enable ANSI escape codes.
     SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), 0x0005);
     SetConsoleMode(GetStdHandle(STD_ERROR_HANDLE),  0x0005);
-    #endif // _WIN32
+#endif // _WIN32
     
     bool pause = false;
     
@@ -102,7 +103,7 @@ void Program::main(int argc, char** argv) {
         startingRoomCandidates.push_front(&room);
     }
     Room& startingRoom = *gen.getRandom().pick(startingRoomCandidates);
-    #if ROOM_TEST_MODE
+#if ROOM_TEST_MODE
     std::string roomFile;
     std::string roomPath = "Resources/Rooms/";
     Program::log(Log::Trace, "RoomTestMode") << "Please enter room filename to test:" << std::endl;
@@ -110,9 +111,9 @@ void Program::main(int argc, char** argv) {
     std::cin >> roomFile;
     roomPath.append(roomFile);
     gen.generateRoomLayoutFromFile(startingRoom, roomPath.c_str());
-    #else
+#else
     gen.generateRoomLayoutFromFile(startingRoom, "Resources/Rooms/special_00000101_spawnpoint.rrm");
-    #endif
+#endif
     
     Texture entitiesTexture;
     entitiesTexture.loadFromFile("Resources/Spritesheets/Entities.png");
@@ -120,14 +121,16 @@ void Program::main(int argc, char** argv) {
     player.setPosition(startingRoom.x * 640.0f + (320.0f - 8.0f), startingRoom.y * 368.0f + (188.0f - 15.0f));
     
     Clock clock;
+#if GAME_LOOP_MODE == 0
     Time accumulator;
+#endif
     const Time optimalTime = seconds(1.0f / 60.0f);
     while (window.isOpen()) {
         {
             Event e;
             while (window.pollEvent(e)) {
-                #pragma GCC diagnostic push
-                #pragma GCC diagnostic ignored "-Wswitch"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wswitch"
                 switch (e.type) {
                     case Event::Closed:
                         Program::log(Log::Info) << "Window closed, shutting down." << std::endl;
@@ -152,23 +155,38 @@ void Program::main(int argc, char** argv) {
                         break;
                 }
                 player.event(e);
-                #pragma GCC diagnostic pop
+#pragma GCC diagnostic pop
             }
         }
         
+#if GAME_LOOP_MODE == 1
+        Time frameTime = clock.restart();
+        float delta = frameTime / optimalTime;
+        int substeps = 1;
+        if (delta >= 1.5f) {
+            delta /= 2;
+            substeps *= 2;
+        }
+        for (; substeps > 0; substeps--) {
+#endif
+#if GAME_LOOP_MODE == 0
         while (accumulator >= optimalTime) {
-            player.update(1.0f);
-            #if ROOM_TEST_MODE
-            camera.update(1.0f, Vector2f(startingRoom.x * 640.0f + 320.0f, startingRoom.y * 368.0f + 188.0f));
-            #else
-            #if CAMERA_MODE == 0
-            camera.update(1.0f, player.getPosition() + Vector2f(player.getTextureRect().width / 2, player.getTextureRect().height / 2) /* + Vector2f(Mouse::getPosition(window) / WINDOWED_SCALE - Vector2i(320, 180)) / 4.0f */);
-            #elif CAMERA_MODE == 1
-            camera.update(1.0f, Vector2f(std::floor((player.getPosition().x + player.getTextureRect().width / 2) / 640.0f) * 640.0f + 320.0f, std::floor((player.getPosition().y + player.getTextureRect().height - 8) / 368.0f) * 368.0f + 188.0f));
-            #endif
-            #endif
+            float delta = 1.0f;
+#endif
+            player.update(delta);
+#if ROOM_TEST_MODE
+            camera.update(delta, Vector2f(startingRoom.x * 640.0f + 320.0f, startingRoom.y * 368.0f + 188.0f));
+#else
+#if CAMERA_MODE == 0
+            camera.update(delta, player.getPosition() + Vector2f(player.getTextureRect().width / 2, player.getTextureRect().height / 2) /* + Vector2f(Mouse::getPosition(window) / WINDOWED_SCALE - Vector2i(320, 180)) / 4.0f */);
+#elif CAMERA_MODE == 1
+            camera.update(delta, Vector2f(std::floor((player.getPosition().x + player.getTextureRect().width / 2) / 640.0f) * 640.0f + 320.0f, std::floor((player.getPosition().y + player.getTextureRect().height - 8) / 368.0f) * 368.0f + 188.0f));
+#endif
+#endif
             minimap.setPosition(std::floor((camera.getFocalPoint().x) / 640.0f) * -8 + 24, std::floor((camera.getFocalPoint().y) / 368.0f) * -8 + 24);
+#if GAME_LOOP_MODE == 0
             accumulator -= optimalTime;
+#endif
         }
         
         {
@@ -188,7 +206,9 @@ void Program::main(int argc, char** argv) {
         window.draw(minimap);
         window.display();
         
+#if GAME_LOOP_MODE == 0
         accumulator += clock.restart();
+#endif
     }
     if (pause) system("pause");
 }
