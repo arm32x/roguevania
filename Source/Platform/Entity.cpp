5b0a0cf9f65fc8b2fe0c4fb16afc3d6490529f8c
ARM32 <arm32.x@gmail.com>
2019-06-17 16:47:44 -0400

    Automatic commit by Code::Blocks.

--- Source/Platform/Entity.cpp
+++ Source/Platform/Entity.cpp
@@ -92,7 +92,7 @@ void Entity::update(float delta) {
                 if (velocity.y > 0.0f && !Keyboard::isKeyPressed(Keyboard::S)) {
                     onGround = true;
                     float amountMoved;
-                    for (amountMoved = 0.0f; (mode == CollisionMode::SemiSolid) && std::abs(amountMoved) <= std::abs(velocity.y * delta - 2) + 1; amountMoved += velocity.y >= 0.0f ? -increment : increment) {
+                    for (amountMoved = 0.0f; (mode == CollisionMode::SemiSolid) && std::abs(amountMoved) <= std::abs(velocity.y * delta); amountMoved += velocity.y >= 0.0f ? -increment : increment) {
                         move(0.0f, velocity.y >= 0.0f ? -increment : increment);
                         setVelocity(velocity.x, 0.0f);
                         mode = collider->prioritizeTileModes(collider->getTilesTouching(*this));
