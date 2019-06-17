c9e3d3bec69775ae618f5a04bea20f2b26198956
ARM32 <arm32.x@gmail.com>
2019-06-17 14:15:31 -0400

    Automatic commit by Code::Blocks.

--- Source/Program.cpp
+++ Source/Program.cpp
@@ -36,7 +36,7 @@ using stx::nullopt;
 #define WINDOWED_SCALE 0
 #define CAMERA_MODE 0
 #define ROOM_TEST_MODE 0
-#define GAME_LOOP_MODE 0
+#define GAME_LOOP_MODE 1
 
 #if WINDOWED_SCALE == 0
 RenderWindow Program::window(VideoMode(VideoMode::getDesktopMode().width, VideoMode::getDesktopMode().width * (9.0 / 16.0)), "Roguevania (working title)", Style::Fullscreen);
