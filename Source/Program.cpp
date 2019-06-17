7594d7e4444ec688bc8169cf954f7ffc88f2dd3f
ARM32 <arm32.x@gmail.com>
2019-06-17 13:43:35 -0400

    Automatic commit by Code::Blocks.

--- Source/Program.cpp
+++ Source/Program.cpp
@@ -197,6 +197,7 @@ int main(int argc, char** argv) {
     try {
         Program::main(argc, argv);
     } catch (...) {
+        Program::window.close();
         system("pause");
         throw;
     }
