70882125ab8c57baf70d1cb9011225e9972be470
ARM32 <arm32.x@gmail.com>
2019-06-17 21:47:10 -0400

    Automatic commit by Code::Blocks.

--- Source/Program.cpp
+++ Source/Program.cpp
@@ -58,6 +58,7 @@ void Program::main(int argc, char** argv) {
     constexpr uint16_t width = 32, height = 16;
     
     MapGenerator gen;
+    gen.setSeed(11025724564705954077u);
     
     Program::log(Log::Trace, "MapGenerator") << "Generating map..." << std::endl;
     Program::log(Log::Info, "MapGenerator") << "Using seed " << gen.getSeed() << "u." << std::endl;
