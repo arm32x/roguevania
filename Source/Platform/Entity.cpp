e0a319f68431e9b352b8defa6dba9118e66e226d
ARM32 <arm32.x@gmail.com>
2019-06-19 17:51:29 -0400

    Automatic commit by Code::Blocks.

--- Source/Platform/Entity.cpp
+++ Source/Platform/Entity.cpp
@@ -121,7 +121,6 @@ void Entity::update(float delta) {
                 }
                 break;
             }
-            // TODO:  Fix falling through semisolid platforms when rubbing against solid walls.
             case CollisionMode::SemiSolid: {
                 Vector2f velocity = getVelocity();
                 if (velocity.y > 0.0f) {
@@ -132,14 +131,11 @@ void Entity::update(float delta) {
                         setVelocity(velocity.x, 0.0f);
                         mode = collider->prioritizeTileModes(collider->getTilesTouching(*this));
                     }
-                    if (mode == CollisionMode::SemiSolid) {
+                    if (mode == CollisionMode::SemiSolid || Keyboard::isKeyPressed(Keyboard::S)) {
                         onGround = false;
                         move(0.0f, -amountMoved);
                         setVelocity(velocity.x, velocity.y);
                     }
-                }
-                if (mode != CollisionMode::SemiSolid && Keyboard::isKeyPressed(Keyboard::S)) {
-                    setVelocity(velocity.x, 0.0f);
                 }
                 break;
             }
