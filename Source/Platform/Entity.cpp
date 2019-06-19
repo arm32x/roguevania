9c4fabdc83322a8bfa3221b51bedd2103a2ec22b
ARM32 <arm32.x@gmail.com>
2019-06-19 17:48:32 -0400

    Automatic commit by Code::Blocks.

--- Source/Platform/Entity.cpp
+++ Source/Platform/Entity.cpp
@@ -132,12 +132,12 @@ void Entity::update(float delta) {
                         setVelocity(velocity.x, 0.0f);
                         mode = collider->prioritizeTileModes(collider->getTilesTouching(*this));
                     }
-                    if (mode == CollisionMode::SemiSolid || Keyboard::isKeyPressed(Keyboard::S)) {
+                    if (mode == CollisionMode::SemiSolid) {
                         onGround = false;
                         move(0.0f, -amountMoved);
-                    }
-                    if (mode == CollisionMode::SemiSolid && !Keyboard::isKeyPressed(Keyboard::S)) {
                         setVelocity(velocity.x, velocity.y);
+                    } else if (Keyboard::isKeyPressed(Keyboard::S)) {
+                        setVelocity(velocity.x, 0.0f);
                     }
                 }
                 break;
