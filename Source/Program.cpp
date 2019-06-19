f914af087747fa2bdf8f1769ce121c11cab214b4
ARM32 <arm32.x@gmail.com>
2019-06-19 17:36:20 -0400

    Automatic commit by Code::Blocks.

--- Source/Program.cpp
+++ Source/Program.cpp
@@ -34,7 +34,7 @@ using namespace Roguevania::Utilities;
 using namespace sf;
 using stx::nullopt;
 
-#define WINDOWED_SCALE 1
+#define WINDOWED_SCALE 0
 #define CAMERA_MODE 1
 #define ROOM_TEST_MODE 0
 #define GAME_LOOP_MODE 1
