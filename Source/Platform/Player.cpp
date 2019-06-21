a8a7b3f57e01ea33985072728e97fb511dc44ba3
ARM32 <arm32.x@gmail.com>
2019-06-20 20:37:21 -0400

    Automatic commit by Code::Blocks.

--- Source/Platform/Player.cpp
+++ Source/Platform/Player.cpp
@@ -68,7 +68,7 @@ void Player::event(const Event& e) {
                             Bullet::bullets.push_back(new Bullet(Entity::spritesheet, IntRect(22, 34,  4, 12), getPosition().x + 6.0f, getPosition().y + 4.0f,  0.0f,  1.0f));
                             break;
                         case 5: // down-left
-                            Bullet::bullets.push_back(new Bullet(Entity::spritesheet, IntRect(35, 35, 10, 10), getPosition().x + 0.0f, getPosition().y + 6.0f, -1.0f,  1.0f));
+                            Bullet::bullets.push_back(new Bullet(Entity::spritesheet, IntRect(51, 35, 10, 10), getPosition().x + 0.0f, getPosition().y + 6.0f, -1.0f,  1.0f));
                             break;
                         case 6: // left
                             Bullet::bullets.push_back(new Bullet(Entity::spritesheet, IntRect( 2, 38, 12,  4), getPosition().x + 0.0f, getPosition().y + 6.0f, -1.0f,  0.0f));
